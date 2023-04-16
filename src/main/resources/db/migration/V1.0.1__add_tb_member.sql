e63c7458777dbe0a57ce81b1a1792e1323b61125
Ropung <563710@naver.com>
2023-04-16 23:47:41 +0900

    init database with flyway, docker-compose

---
+++ src/main/resources/db/migration/V1.0.1__add_tb_member.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS jpa_basic.member (
+    id              UUID,
+    email           VARCHAR(30)         NOT NULL,
+    password        VARCHAR(255)        NOT NULL,
+    nickname        VARCHAR(30)         NOT NULL,
+    status          VARCHAR(30)         NOT NULL,
+    created_at      DATE                NOT NULL,
+
+    CONSTRAINT      pk_member_id        PRIMARY KEY(id)
+);
+
+CREATE UNIQUE INDEX udx_member_email ON jpa_basic.member(email);
